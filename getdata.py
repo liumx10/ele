725328f937bc934177922de3f2e8db1e8d0eb607
liumx10 <liumengxing2010@qq.com>
2015-12-12 13:59:24 +0800

    save

---
+++ getdata.py
@@ -0,0 +1,46 @@
+import requests 
+import json
+
+def get_shop():
+	r = requests.get('http://115.159.159.158:9200/hackathon/restaurant/_search')
+	res = json.loads(r.text)
+	size = res['hits']['total']
+
+	r = requests.get('http://115.159.159.158:9200/hackathon/restaurant/_search?from=0&size='+str(size))
+	return json.loads(r.text)
+def get_menu():
+	r = requests.get('http://115.159.159.158:9200/hackathon/menu/_search')
+	res = json.loads(r.text)
+	size = res['hits']['total']
+
+	r = requests.get('http://115.159.159.158:9200/hackathon/menu/_search?from=0&size='+str(size))
+	return json.loads(r.text)
+
+def get_order(restaurant_id):
+	baseurl  = "http://115.159.159.158:9200/hackathon/menu/_search?q=restaurant_id:"+str(restaurant_id)
+	r = requests.get(baseurl)
+	res = json.loads(r.text)
+	size = res['hits']['hits']
+
+	r = requests.get(baseurl+"&from=0&size="+str(size))
+	return json.loads(r.text)
+
+def main():
+	shop = []
+	menu = []
+	order = []
+
+	#shop = get_shop()
+	#menu = get_menu()
+	order = get_order(456941)
+
+	print "========= shop number =========="
+	#print len(shop['hits']['hits'])
+	print "========= menu number =========="
+	#print len(menu['hits']['hits'])
+	print "========= order number ========="
+	print len(order)
+
+
+if __name__ == "__main__":
+	main()
